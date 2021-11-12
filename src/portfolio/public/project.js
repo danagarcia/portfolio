ac395d106f274bd6f70b8fededa33a5eee6a2219
danagarcia <54691220+danagarcia@users.noreply.github.com>
2021-11-11 23:35:54 -0800

    Adding portfolio site

---
+++ src/portfolio/public/project.js
@@ -0,0 +1,12 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "project (1).png",
+    },
+    {
+        name: "project two",
+        tags: "#javascript, #fullstack",
+        image: "project (2).png",
+    }
+]
