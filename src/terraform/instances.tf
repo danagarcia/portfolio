f70ebf6a8ab91284f087ed3686058860abaec503
Dana Garcia <dana_sean_garcia@live.com>
2021-11-13 21:49:32 -0800

    add support for single ad site

--- src/terraform/instances.tf
+++ src/terraform/instances.tf
@@ -21,10 +21,7 @@ resource "oci_core_instance" "web-be-vm1" {
 }
 
 resource "oci_core_instance" "web-be-vm2" {
-  availability_domain = try(
-    data.oci_identity_availability_domains.ads.availability_domains[1].name,
-    data.oci_identity_availability_domains.ads.availability_domains[0].name
-  )
+  availability_domain = length(data.oci_identity_availability_domains.ads.availability_domains) >= 2 ? data.oci_identity_availability_domains.ads.availability_domains[1].name : data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_ocid
   display_name        = "web-be-vm2"
   shape               = var.instance_shape
@@ -35,7 +32,7 @@ resource "oci_core_instance" "web-be-vm2" {
   }
 
   create_vnic_details {
-    subnet_id      = data.oci_core_subnet.publicsnad2.id
+    subnet_id      = length(data.oci_identity_availability_domains.ads.availability_domains) >= 2 ? data.oci_core_subnet.publicsnad2.id : data.oci_core_subnet.publicsnad1.id
     hostname_label = "web-be-vm2"
   }
 
